028e2ff9590ccdb3066fe161f808389bf2a443fc
Lanode <lanode@mail.ru>
2020-09-28 17:19:19 +0300

    added more banners

--- random_banner.js
+++ random_banner.js
@@ -1,12 +1,11 @@
 
-var random_images_array = ["1.jpg", "2.jpg", "3.jpg"];
+//var random_images_array = ["1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.jpg", "6.jpg", "7.jpg", "8.jpg"];
+var images_count = 12;
 
 function ready() {
     var path = 'data/images/banners/'; // default path here
-    var num = Math.floor(Math.random() * random_images_array.length);
-    var img = random_images_array[num];
-    var pathImg = path + img;
-    document.getElementById("banner").style.backgroundImage = `url(${pathImg})`;
+    var imgNum = Math.floor(Math.random() * images_count)+1;
+    document.getElementById("banner").style.backgroundImage = `url(${path+imgNum}.jpg)`;
     //document.getElementById("banner").style.backgroundColor = "red";
 }
 
